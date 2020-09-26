bf9a7d4e88c1d3e6e076567274089a566e2e5dd1
Andrew Bui <andrew.bui.au@gmail.com>
2020-09-26 21:01:47 +1000

    initialised tty-prompt and created game menu using the tty-prompt gem

--- modules.rb
+++ modules.rb
@@ -1,44 +1,56 @@
 
 
-# Story Elements Module
-module StoryElements
 
-    def self.story_intro
-        sleep 5
+# Story Elements Module
+module GameEngine
+
+    def self.player_name
+        puts LayoutElements::INVISIBLE_SEPARATOR
+        print "To start game, please input player name here: "
+        player_name = gets.chomp
+        player_name.to_s
+        player_name.capitalize!
+        GameEngine.story_intro(player_name)
+    end
+
+    def self.story_intro(player_name)
+        sleep 1
         puts "It is the year 2088"
         sleep 1
         puts "Advancements in technology has lead to an influx of a new generation of body modification, called augmentations.".light_cyan
         sleep 1
+        puts "You are highly trained super augmented CIA Agent #{player_name.light_cyan} Caddel."
+        sleep 1
     end
 
-    begin
-        def self.the_augmented_title
-            puts "----------------------------------------------------------------------------------------------------------"
-            puts " "
-            puts " "
-            puts " "
-            puts " "
-            puts " "
-            puts " "
-            puts " "
-            puts "████████╗██╗░░██╗███████╗  ░█████╗░██╗░░░██╗░██████╗░███╗░░░███╗███████╗███╗░░██╗████████╗███████╗██████╗░".blue
-            puts "╚══██╔══╝██║░░██║██╔════╝  ██╔══██╗██║░░░██║██╔════╝░████╗░████║██╔════╝████╗░██║╚══██╔══╝██╔════╝██╔══██╗".light_blue
-            puts "░░░██║░░░███████║█████╗░░  ███████║██║░░░██║██║░░██╗░██╔████╔██║█████╗░░██╔██╗██║░░░██║░░░█████╗░░██║░░██║".light_blue
-            puts "░░░██║░░░██╔══██║██╔══╝░░  ██╔══██║██║░░░██║██║░░╚██╗██║╚██╔╝██║██╔══╝░░██║╚████║░░░██║░░░██╔══╝░░██║░░██║".light_cyan.blink
-            puts "░░░██║░░░██║░░██║███████╗  ██║░░██║╚██████╔╝╚██████╔╝██║░╚═╝░██║███████╗██║░╚███║░░░██║░░░███████╗██████╔╝".blue
-            puts "░░░╚═╝░░░╚═╝░░╚═╝╚══════╝  ╚═╝░░╚═╝░╚═════╝░░╚═════╝░╚═╝░░░░░╚═╝╚══════╝╚═╝░░╚══╝░░░╚═╝░░░╚══════╝╚═════╝░".blue
-            puts " "
-            puts " "
-            puts "                                                By aBuiDev                                                "
-            puts " "
-            puts " "
-            puts " "
-            puts " "
-            puts " "
-            puts "----------------------------------------------------------------------------------------------------------"
-        end
-    rescue
-        puts "Error occured"
+    def self.about_the_augmented
+        puts "The Augmented is a text-based game that will prompt users to make emotionally driven decisions that will affect the story's outcome. The Augmented is an action RPG set in the year 2088 where humanity has begun to advance the technologies of being able to transpose machine with man via body augmentations. The Augmented universe is based loosely on Eidos' original Deus Ex PC game. The Augmented will take place in New York, on Staten Island - after highly trained and highly advanced unknown terrorists destroy the statue of liberty. The player will take on the role of Agent Caddel, a highly trained and heavily augmented CIA agent - because his body is now over 75% machine, he also fights an internal battle of self-discovery and self-identity."
+    end
+
+    def self.the_augmented_title
+        puts "----------------------------------------------------------------------------------------------------------"
+        puts " "
+        puts " "
+        puts " "
+        puts " "
+        puts " "
+        puts " "
+        puts " "
+        puts "████████╗██╗░░██╗███████╗  ░█████╗░██╗░░░██╗░██████╗░███╗░░░███╗███████╗███╗░░██╗████████╗███████╗██████╗░".blue
+        puts "╚══██╔══╝██║░░██║██╔════╝  ██╔══██╗██║░░░██║██╔════╝░████╗░████║██╔════╝████╗░██║╚══██╔══╝██╔════╝██╔══██╗".light_blue
+        puts "░░░██║░░░███████║█████╗░░  ███████║██║░░░██║██║░░██╗░██╔████╔██║█████╗░░██╔██╗██║░░░██║░░░█████╗░░██║░░██║".light_blue
+        puts "░░░██║░░░██╔══██║██╔══╝░░  ██╔══██║██║░░░██║██║░░╚██╗██║╚██╔╝██║██╔══╝░░██║╚████║░░░██║░░░██╔══╝░░██║░░██║".light_cyan.blink
+        puts "░░░██║░░░██║░░██║███████╗  ██║░░██║╚██████╔╝╚██████╔╝██║░╚═╝░██║███████╗██║░╚███║░░░██║░░░███████╗██████╔╝".blue
+        puts "░░░╚═╝░░░╚═╝░░╚═╝╚══════╝  ╚═╝░░╚═╝░╚═════╝░░╚═════╝░╚═╝░░░░░╚═╝╚══════╝╚═╝░░╚══╝░░░╚═╝░░░╚══════╝╚═════╝░".blue
+        puts " "
+        puts " "
+        puts "                                                By aBuiDev                                                ".light_blue
+        puts " "
+        puts " "
+        puts " "
+        puts " "
+        puts " "
+        puts "----------------------------------------------------------------------------------------------------------"
     end
 
 end
@@ -47,13 +59,29 @@ end
 
 # Layout & Structuring Elements
 module LayoutElements
-    VISIBLE_SEPARATOR = "----------------------------------------"
+    VISIBLE_SEPARATOR = "----------------------------------------------------------------------------------------------------------"
     INVISIBLE_SEPARATOR = " "
 
-    def LayoutElements.clear
+    def self.clear
         print "\e[2J\e[f"
     end
 
+    def self.game_menu
+        prompt = TTY::Prompt.new
+        game_menu_items = %w(Start_Game About_The_Augmented Exit_Game)
+        game_menu_user_selection = prompt.select("Game Menu: ".light_blue, game_menu_items)
+        if game_menu_user_selection == "Start_Game"
+            LayoutElements.clear
+            GameEngine.player_name
+        elsif game_menu_user_selection == "About_The_Augmented"
+            LayoutElements.clear
+            GameEngine.about_the_augmented
+        elsif game_menu_user_selection == "Exit_Game"
+            puts "Thank you for playing The Augmented"
+        end 
+    end
 end
+
+
 
 
--- the_augmented.rb
+++ the_augmented.rb
@@ -1,22 +1,36 @@
 # Dependencies
 require 'time'
 require 'colorize'
+require 'tty-prompt'
 require_relative './animations.rb'
 require_relative './modules.rb'
 
 
+# TTY-Prompt
+prompt = TTY::Prompt.new
+
+
+# Variables | Separators
+invisible_separator = LayoutElements::INVISIBLE_SEPARATOR
+visible_separator = LayoutElements::VISIBLE_SEPARATOR
+
 
 LayoutElements.clear
+GameEngine::the_augmented_title
+
+
+sleep 1
+LayoutElements.clear
+LayoutElements.game_menu
+
+
+player_class = %w(Heavy Assassin)
+prompt.select("Select your class: ", player_class)
 
 
 
-StoryElements::the_augmented_title
 
 
-
-puts LayoutElements::VISIBLE_SEPARATOR
-StoryElements.story_intro
-puts LayoutElements::VISIBLE_SEPARATOR
 
 
 
