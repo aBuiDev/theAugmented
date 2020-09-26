668426d85d34dd5c35f30d1d0be8bb1e104fc5eb
Andrew Bui <andrew.bui.au@gmail.com>
2020-09-26 11:02:37 +1000

    setup initial MVC layout, created .gitignore file to hide gems and added rescues

---
+++ modules.rb
@@ -0,0 +1,12 @@
+
+
+# Story Elements Module
+module StoryElements
+    SEPARATOR = "----------------------------------------"
+    def self.story_intro
+        puts "It is the year 2088"
+        sleep 1
+        puts "Advancements in technology has lead to an influx of a new generation of body modification, called augmentations.".light_cyan
+        sleep 1
+    end
+end
