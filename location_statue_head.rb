16169114bf90fd177f407fb4c0009d28a0d01023
Andrew Bui <andrew.bui.au@gmail.com>
2020-10-02 16:33:00 +1000

    added story elements for statue head area and battle

--- location_statue_head.rb
+++ location_statue_head.rb
@@ -81,8 +81,162 @@ module GameLocations
         puts InterfaceElements::INVISIBLE_SEPARATOR
         puts "and scan the surrounding area from this vantage point."
         sleep 1
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        puts "#{"| Eagle Eye Vision Augmentation |".green} locks onto Henderson!,"
+        sleep 1
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        puts "who is engaged in an earth-shattering augmented battle with Terrorist Commander Hermes Himler..."
+        sleep 1
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        puts "an extremely brutal and ultra-augmented enemy of the world."
+        sleep 1
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        puts "It is rumoured that only 35% of his brain remains human."
+        sleep 1
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        puts InterfaceElements::VISIBLE_SEPARATOR
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        # Interface Controls --------------------------------------------------------------------
+        prompt = TTY::Prompt.new
+        continue_only = [
+            {name: 'Continue', value: 1},
+        ]
+        continue_command = prompt.select("Press Continue When Ready:".light_cyan, continue_only)
+        # Control Conditionals ------------------------------------------------------------------
+        case continue_command
+        when 1
+            GameLocations.liberty_statue_head_part_two(new_player)
+        end
         InterfaceElements.statue_head_game_controls(new_player)
     end
+    
+    # Location: Liberty Statue Head Game Over Part Two
+    def self.liberty_statue_head_part_two(new_player)
+        InterfaceElements.clear
+        sleep 1
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        puts "Liberty Statue Head".light_cyan
+        puts InterfaceElements::VISIBLE_SEPARATOR
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts "Henderson is also a #{new_player.class_name.light_cyan} class, super-agent..."
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts "however, he alone cannot defeat Terrorist Commander Hermes Himler..."
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts "At least two of you, heavy weaponry and pure grit will be needed to defeat Himler."
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts "You hope that the weapons you are carrying at the moment are enough..."
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts "you have no other option but to aid Henderson."
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts "..."
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts "You activate #{"| Nano Teleportation Augmentation |".light_cyan}"
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts "..."
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        puts InterfaceElements::VISIBLE_SEPARATOR
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        # Interface Controls --------------------------------------------------------------------
+        prompt = TTY::Prompt.new
+        continue_only = [
+            {name: 'Continue', value: 1},
+        ]
+        continue_command = prompt.select("Press Continue When Ready:".light_cyan, continue_only)
+        # Control Conditionals ------------------------------------------------------------------
+        case continue_command
+        when 1
+            GameLocations.liberty_statue_head_part_three(new_player)
+        end
+    end
+
+    # Location: Liberty Statue Head Part Three
+    def self.liberty_statue_head_part_three(new_player)
+        InterfaceElements.clear
+        Animations.alert_animation
+        sleep 1
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        puts "Liberty Statue Head".light_cyan
+        puts InterfaceElements::VISIBLE_SEPARATOR
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts "Upon engaging #{"| Nano Teleportation Augmentation |".light_cyan}..."
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts "Your systems alert you to a #{"stealth attack".red}!"
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts "You instantly activate #{"| Titan Shield Augmentation |".green} and swiftly turn around to see..."
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts "Terrorist Sub-Commander Helana Hellspawn materialise in front of you..."
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts "a beautiful assassin augmented to output death."
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts "As her cloaking augmentation disengages, you realise the GEP Gun you had picked up from the mutated"
+        puts "and augmented sewer rat had shielded you from what would have been an instant kill stealth attack."
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        # Interface Controls --------------------------------------------------------------------
+        prompt = TTY::Prompt.new
+        continue_only = [
+            {name: 'Continue', value: 1},
+        ]
+        continue_command = prompt.select("Press Continue When Ready:".light_cyan, continue_only)
+        # Control Conditionals ------------------------------------------------------------------
+        case continue_command
+        when 1
+            GameLocations.liberty_statue_head_part_four(new_player)
+        end
+    end
+
+    # Location: Liberty Statue Head Game Over Part Four
+    def self.liberty_statue_head_part_four(new_player)
+        InterfaceElements.clear
+        Animations.alert_animation
+        sleep 1
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        puts "Liberty Statue Head".light_cyan
+        puts InterfaceElements::VISIBLE_SEPARATOR
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts "Fueled by both pure rage and desperation to save your brother, you..."
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts "activate #{"| Nano Arm Blade Augmentation |".green}!"
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts "activate #{"| Enhanced Speed Augmentation |".green}!"
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts "activate #{"| Micro-fibral Muscle Augmentation |".green}!"
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts "With an incredibly swift motion - enhanced by augmentations - you launch a powerful augmented melee attack..."
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts "launching Terrorist Sub-Commander Helena Hellspawn into the air..."
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts "her life fading away before her body hits the ground, rolling to towards the edge before being caught by a"
+        puts "liberty crown spike."
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        sleep 1
+        puts "As her body lays lifeless over the edge of the crown, you move towards it..."
+        puts InterfaceElements::INVISIBLE_SEPARATOR
+        InterfaceElements.statue_head_game_controls(new_player)
+    end
+        
 
 
 
